42e23f9b88522c4cc70ac664f523b6d48cd1cdd7
unknown <Christopher.Tully@T430_10380.cardionet.com>
2015-02-09 10:17:11 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+1. WEvent / Event Toggle
+2. Column Coloring
+3. Red row coloring / onhover
+4. Search by Prefix
+*/
+/*
+var row = document.getElementsByTagName("td");
+for (var i = 0; i < row.length; i{
+	row[i].style.backgroundColor = "red";
+};
+*/
